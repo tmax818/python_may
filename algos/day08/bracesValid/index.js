2e244d561b514d58ce9ec042de8c9fc643a94cf0
tmax818 <tylermaxwell661@gmail.com>
2022-05-12 10:13:44 -0700

    update

--- algos/day08/bracesValid/index.js
+++ algos/day08/bracesValid/index.js
@@ -12,6 +12,31 @@ const expected2 = false;
 const str3 = "A(1)s[O (n]0{t) 0}k";
 const expected3 = false;
 
+function parensValid(str) {
+    let countP = 0
+    let countC = 0
+    let countB = 0
+    brackets = {')': '(', ']': '[', '}': '{'}
+    for(var i = 0; i < str.length;i++){
+        if(str[i] === '(' ){
+            countP++
+        }
+        if(str[i] === '{' ){
+            countC++
+        }
+        if(str[i] === '[' ){
+            countB++
+        }
+        if(str[i] === ')' || str[i] === '}' || str[i] === ']'){
+            count--
+        }
+        if(count < 0){
+            return false
+        }
+    }
+    return count === 0
+}
+
 /**
  * Determines whether the string's braces, brackets, and parenthesis are valid
  * based on the order and amount of opening and closing pairs.
@@ -20,41 +45,27 @@ const expected3 = false;
  * @param {string} str
  * @returns {boolean} Whether the given strings braces are valid.
  */
-function bracesValid(str) {}
+// function bracesValid(str) {
+//     for(let i = 0; i > str.length; i++){
 
-// console.log(bracesValid(str1))
-// console.log(bracesValid(str2))
-// console.log(bracesValid(str3))
+//     }
+// }
+
+console.log(parensValid(str1))
+console.log(parensValid(str2))
+console.log(parensValid(str3))
 
 
-/**
- const str1 = "W(a{t}s[o(n{ c}o)m]e )h[e{r}e]!";
- * - Time: O(n * m) where n = str.length and m = opens.length,
- *    since opens.length is constant length of 3 -> O(3n) -> O(n) linear.
- * - Space: O(n) linear.
- * @param {string} str
- * @returns {boolean}
- */
-function bracesValid2(str = "") {
-    const stack = [];
-    const opens = "({[";
-    const closeToOpen = { ")": "(", "}": "{", "]": "[" };
-    
-    for (let i = 0; i < str.length; i++) {
-        console.log("stack=", stack, "i=", str[i])
-        if (opens.includes(str[i])) {
-            stack.push(str[i]);
-        } else if (str[i] in closeToOpen) {
-            if (closeToOpen[str[i]] === stack[stack.length - 1]) {
-                stack.pop();
-            } else {
-                return false;
-            }
-        }
-    }
-    return stack.length === 0;
-}
+// /**
+//  const str1 = "W(a{t}s[o(n{ c}o)m]e )h[e{r}e]!";
+//  * - Time: O(n * m) where n = str.length and m = opens.length,
+//  *    since opens.length is constant length of 3 -> O(3n) -> O(n) linear.
+//  * - Space: O(n) linear.
+//  * @param {string} str
+//  * @returns {boolean}
+//  */
+// function bracesValid2(str = "") {}
 
-console.log(bracesValid2(str1))
-console.log(bracesValid2(str2))
-console.log(bracesValid2(str3))
+// console.log(bracesValid2(str1))
+// console.log(bracesValid2(str2))
+// console.log(bracesValid2(str3))
--- algos/day08/parensValid/index.js
+++ algos/day08/parensValid/index.js
@@ -3,7 +3,10 @@ Parens Valid
 Given a str that has parenthesis in it
 return whether the parenthesis are valid
 */
-")("
+// count
+
+//")("
+
 const str1 = "Y(3(p)p(3)r)s";
 const expected1 = true;
 
@@ -19,6 +22,8 @@ const str4 = "a(b))(c";
 const expected4 = false;
 // Explanation: same number of opens and closes but the 2nd closing closes nothing.
 
+const str5 = "(((((((((())))))))))"
+const expected5 = true
 /**
  * Determines whether the parenthesis in the given string are valid.
  * Each opening parenthesis must have exactly one closing parenthesis.
@@ -27,9 +32,24 @@ const expected4 = false;
  * @param {string} str
  * @returns {boolean} Whether the parenthesis are valid.
  */
-function parensValid(str) {}
+function parensValid(str) {
+    let count = 0
+    for(var i = 0; i < str.length;i++){
+        if(str[i] === '('){
+            count++
+        }
+        if(str[i] === ')'){
+            count--
+        }
+        if(str[i] === ')' && count < 0){
+            return false
+        }
+    }
+    return count === 0
+}
 
 console.log(parensValid(str1))
 console.log(parensValid(str2))
 console.log(parensValid(str3))
 console.log(parensValid(str4))
+console.log(parensValid(str5))
