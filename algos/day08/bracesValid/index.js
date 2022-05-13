ea99d014023dfc43099534167d1450c61f8b3148
tmax818 <tylermaxwell661@gmail.com>
2022-05-13 09:33:08 -0700

    update

--- algos/day08/bracesValid/index.js
+++ algos/day08/bracesValid/index.js
@@ -12,31 +12,6 @@ const expected2 = false;
 const str3 = "A(1)s[O (n]0{t) 0}k";
 const expected3 = false;
 
-function parensValid(str) {
-    let countP = 0
-    let countC = 0
-    let countB = 0
-    brackets = {')': '(', ']': '[', '}': '{'}
-    for(var i = 0; i < str.length;i++){
-        if(str[i] === '(' ){
-            countP++
-        }
-        if(str[i] === '{' ){
-            countC++
-        }
-        if(str[i] === '[' ){
-            countB++
-        }
-        if(str[i] === ')' || str[i] === '}' || str[i] === ']'){
-            count--
-        }
-        if(count < 0){
-            return false
-        }
-    }
-    return count === 0
-}
-
 /**
  * Determines whether the string's braces, brackets, and parenthesis are valid
  * based on the order and amount of opening and closing pairs.
@@ -45,15 +20,32 @@ function parensValid(str) {
  * @param {string} str
  * @returns {boolean} Whether the given strings braces are valid.
  */
-// function bracesValid(str) {
-//     for(let i = 0; i > str.length; i++){
+function bracesValid(str) {
+    const stack = []
+    const opens = "({[";
+    const closeToOpen = { ")": "(", "}": "{", "]": "[" };
+    for(let i = 0; i < str.length; i++){
+        if(opens.includes(str[i])){
+            console.log('stackbefore = ', stack, "strEl =", str[i])
+            stack.push(str[i])
+            console.log('stack = ', stack, "strEl =", str[i])
+        } else if (str[i] in closeToOpen) {
+            if(closeToOpen[str[i]] === stack[stack.length -1]){
+                console.log("stackafter=", stack, "i=", str[i])
+                stack.pop();
 
-//     }
-// }
+            } else {
+                return false
+            }
+        }
+    }
+    return stack.length === 0;
+}
 
-console.log(parensValid(str1))
-console.log(parensValid(str2))
-console.log(parensValid(str3))
+console.log(bracesValid(str1))
+// console.log(bracesValid2(str2))
+// console.log(bracesValid2(str3))
+
 
 
 // /**
@@ -66,6 +58,8 @@ console.log(parensValid(str3))
 //  */
 // function bracesValid2(str = "") {}
 
-// console.log(bracesValid2(str1))
-// console.log(bracesValid2(str2))
-// console.log(bracesValid2(str3))
+
+
+let mystr = "Hello Yo Han"
+
+console.log(mystr.includes("Hell"))
